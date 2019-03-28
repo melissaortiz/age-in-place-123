e04334618ca61c1bd5455ecba7d04b528c77733c
Laila Elimam <lailaelimam@gmail.com>
2019-03-28 00:46:53 -0500

    NOTE: CHANGE TO THE RUBY VERSION. Please make sure you have 2.6.1 installed (version in Gemfile). Additional HTML and CSS for _header file. HTML & CSS for _footer file. Also addition of routes for header page (routes.rb - need to review this). New Logo added.

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 #root 'welcome#index'
 
+get "home/index" => "home#index"
+get "/about" => "home#about"
+get "/services" => "home#services"
 
 end
